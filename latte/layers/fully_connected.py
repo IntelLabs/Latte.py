deb7b2f2f6192ba0371fa14592406f7f7a62da7c
Leonard Truong <leonardtruong@gmail.com>
2016-04-25 15:05:34 -0700

    Rename file

--- latte/layers/fully_connected.py
+++ latte/layers/fully_connected.py
@@ -1,6 +1,29 @@
 import numpy as np
-from ..neuron import WeightedNeuron
 from ..ensemble import Ensemble
+
+class WeightedNeuron(Neuron):
+    def __init__(self, weights, bias):
+        super().__init__()
+        self.inputs = []
+        self.grad_inputs = []
+
+        self.weights = weights
+        self.grad_weights = np.zeros_like(weights)
+
+        self.bias = bias
+        self.grad_bias = np.zeros_like(bias)
+
+    def forward(self):
+        for i, input_idx in enumerate_mapping(self.inputs):
+            self.value += self.inputs[input_idx] * self.weights[i]
+        self.value += self.bias[0]
+
+    def backward(self):
+        for i, input_idx in enumerate_mapping(self.inputs):
+            self.grad_inputs[input_idx] += self.grad * self.weights[i]
+        for i, input_idx in enumerate_mapping(self.inputs):
+            self.grad_weights[i] += self.grad * self.inputs[input_idx]
+        self.grad_bias[0] += self.grad
 
 def FullyConnectedLayer(net, input_ensemble, num_outputs):
     fan_in = len(input_ensemble)
