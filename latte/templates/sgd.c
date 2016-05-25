a6ab529c59dcf8c3377e4433f3a13e7cc3c78d7f
Leonard Truong <leonardtruong@gmail.com>
2016-05-25 16:30:53 -0700

    Revert sgd change

--- latte/templates/sgd.c
+++ latte/templates/sgd.c
@@ -4,7 +4,7 @@
 
 extern "C"
 void sgd_update(float *__restrict param, float *__restrict grad, float
-        *__restrict hist, float lr, float mom, int length, int batch_size) {
+        *__restrict hist, float lr, float mom, int length, int _num_threads) {
     __assume_aligned(grad, 64);
     __assume_aligned(hist, 64);
     __assume_aligned(param, 64);
@@ -12,7 +12,7 @@ void sgd_update(float *__restrict param, float *__restrict grad, float
 #pragma omp parallel for simd
     for (int i = 0; i < length; i++) {
         float _grad = 0.0;
-        for (long j = 0; j < batch_size; j++) {
+        for (long j = 0; j < _num_threads; j++) {
             _mm_prefetch((char*)(grad + (j + 1) * length + i),_MM_HINT_T0);
             _grad += grad[j * length + i];
         }
