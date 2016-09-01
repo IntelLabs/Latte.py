3bf2fbff81a987698af781cd4ff1838bdc44a00c
anand.venkat <avenkat@PSEPHI01.jf.intel.com>
2016-09-01 08:52:18 -0700

    added LRN

--- examples/GoogLeNet/caffe_script.py
+++ examples/GoogLeNet/caffe_script.py
@@ -10,35 +10,56 @@ Merge((open("train_val.prototxt", 'r').read()), net)
 s = ''
 for layer in range(len(net.layer)):
     if net.layer[layer].type == "Convolution": #convolution    
-       s += str(net.layer[layer].name) + ' = ConvLayer(net, ' + str(net.layer[layer].bottom[0]) + \
+        s += str(net.layer[layer].name) + ' = ConvLayer(net, ' + str(net.layer[layer].bottom[0]) + \
             ', num_filters=' + str(net.layer[layer].convolution_param.num_output)
-       if len(net.layer[layer].convolution_param.kernel_size) != 0 :
-           s += ', kernel=' + str(net.layer[layer].convolution_param.kernel_size[0])
-       if len(net.layer[layer].convolution_param.stride) != 0:
-           s+= ', stride=' + str(net.layer[layer].convolution_param.stride[0])
-       else:
-           s+= ', stride=1'
-       if len(net.layer[layer].convolution_param.pad) != 0:
-           s+= ', pad=' + str(net.layer[layer].convolution_param.pad[0])
-       s += ')\n'
-       
+        if len(net.layer[layer].convolution_param.kernel_size) != 0 :
+            s += ', kernel=' + str(net.layer[layer].convolution_param.kernel_size[0])
+        if len(net.layer[layer].convolution_param.stride) != 0:
+            s+= ', stride=' + str(net.layer[layer].convolution_param.stride[0])
+        else:
+            s+= ', stride=1'
+        if len(net.layer[layer].convolution_param.pad) != 0:
+            s+= ', pad=' + str(net.layer[layer].convolution_param.pad[0])
+        s += ')\n'
+
+    if net.layer[layer].type == "InnerProduct": #InnerProduct/FullyConnected   
+        s += str(net.layer[layer].name) + ' =  FullyConnectedLayer(net, ' + str(net.layer[layer].bottom[0]) + \
+            ', ' + str(net.layer[layer].inner_product_param.num_output) + ')\n'
+           
     if net.layer[layer].type == "ReLU": #relu
-       s += str(net.layer[layer].name) + ' = ReLULayer(net, ' + str(net.layer[layer].bottom[0]) + ')\n'
+        s += str(net.layer[layer].name) + ' = ReLULayer(net, ' + str(net.layer[layer].bottom[0]) + ')\n'
 
     if net.layer[layer].type == "SoftmaxWithLoss": #softmax
-       s += str(net.layer[layer].name) + ' = SoftmaxLossLayer(net, ' + str(net.layer[layer].bottom[0]) +', ' + str(net.layer[layer].bottom[1]) + ')\n'
+        s += str(net.layer[layer].name) + ' = SoftmaxLossLayer(net, ' + str(net.layer[layer].bottom[0]) +', ' + str(net.layer[layer].bottom[1]) + ')\n'
 
-
-
+    if net.layer[layer].type == "LRN": #Local Response Normalization
+        s += str(net.layer[layer].name) + ' = LRNLayer(net, ' + str(net.layer[layer].bottom[0]) +', ' + \
+            str(net.layer[layer].lrn_param.local_size) + ', ' + str(net.layer[layer].lrn_param.beta) + ', ' + \
+            str(net.layer[layer].lrn_param.alpha)+ ')\n'  
+    
     if net.layer[layer].type == "Pooling" and net.layer[layer].pooling_param.pool == caffe_pb2.PoolingParameter.MAX: # max pool
-       s += str(net.layer[layer].name) + ' = MaxPoolingLayer(net, ' + str(net.layer[layer].bottom[0])
-       if net.layer[layer].pooling_param.kernel_size != None:
-              s += ', kernel=' + str(net.layer[layer].pooling_param.kernel_size)
-       if net.layer[layer].pooling_param.stride != None:
-              s+= ', stride=' + str(net.layer[layer].pooling_param.stride)
-       else:
-              s+= ', stride=1'
-       if net.layer[layer].pooling_param.pad != None:
-              s+= ', pad=' + str(net.layer[layer].pooling_param.pad)
-       s += ')\n'
+        s += str(net.layer[layer].name) + ' = MaxPoolingLayer(net, ' + str(net.layer[layer].bottom[0])
+        if net.layer[layer].pooling_param.kernel_size != None:
+            s += ', kernel=' + str(net.layer[layer].pooling_param.kernel_size)
+        if net.layer[layer].pooling_param.stride != None:
+            s+= ', stride=' + str(net.layer[layer].pooling_param.stride)
+        else:
+            s+= ', stride=1'
+        if net.layer[layer].pooling_param.pad != None:
+            s+= ', pad=' + str(net.layer[layer].pooling_param.pad)
+        s += ')\n'
+
+    if net.layer[layer].type == "Pooling" and net.layer[layer].pooling_param.pool == caffe_pb2.PoolingParameter.AVE: # average pool
+        s += str(net.layer[layer].name) + ' = MeanPoolingLayer(net, ' + str(net.layer[layer].bottom[0])
+        if net.layer[layer].pooling_param.kernel_size != None:
+            s += ', kernel=' + str(net.layer[layer].pooling_param.kernel_size)
+        if net.layer[layer].pooling_param.stride != None:
+            s+= ', stride=' + str(net.layer[layer].pooling_param.stride)
+        else:
+            s+= ', stride=1'
+        if net.layer[layer].pooling_param.pad != None:
+            s+= ', pad=' + str(net.layer[layer].pooling_param.pad)
+        s += ')\n'
+    
+
 print(s)
