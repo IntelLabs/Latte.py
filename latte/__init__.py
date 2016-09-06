3dfdd24194f5845d539f90b0e1c7d147c281e41c
anand.venkat <avenkat@psr-knl01.sc.intel.com>
2016-09-05 19:14:19 -0700

    adding lrn and average pooling

--- latte/__init__.py
+++ latte/__init__.py
@@ -2,10 +2,11 @@ from .core import Net
 from .layers.fully_connected import FullyConnectedLayer, FullyConnectedLayerNoBias
 from .layers.memory import MemoryDataLayer
 from .layers.conv import ConvLayer, ConvLayerNoBias
-from .layers.pooling import MaxPoolingLayer
+from .layers.pooling import MaxPoolingLayer, MeanPoolingLayer
 from .layers.relu import ReLULayer
 from .layers.softmax import SoftmaxLossLayer
 from .layers.accuracy import AccuracyLayer
 from .layers.seg_accuracy import SegAccuracyLayer
 from .layers.interpolation import InterpolationLayer
 from .layers.dropout import DropoutLayer
+from .layers.lrn import LRNLayer
