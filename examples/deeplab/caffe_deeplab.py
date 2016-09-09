374d872586cac640b54b7225777352cf58f9ebb5
Naila Farooqui <gtnaila@gmail.com>
2016-09-09 10:20:55 -0700

    Adding DeepLab/CityScapes config files.

--- examples/deeplab/caffe_deeplab.py
+++ examples/deeplab/caffe_deeplab.py
@@ -6,4 +6,20 @@ MODEL = 'CityScapes/model/infinet/train_iter_10000.caffemodel'
 
 net = caffe.Net(CONFIG, MODEL)
 
-[(k,v.data.shape) for k,v in net.blobs.items()]
+for k,v in net.blobs.items():
+    print(k)
+    print(v.data.shape)
+
+
+net.forward()
+
+print('DATA: {}'.format(net.blobs['data'].data.shape))
+print(net.blobs['data'].data)
+print('CONV1_1 WEIGHTS: {}'.format(net.params['conv1_1'][0].data.shape))
+print(net.params['conv1_1'][0].data)
+print('CONV1_1 BIAS: {}'.format(net.params['conv1_1'][1].data.shape))
+print(net.params['conv1_1'][1].data)
+print('CONV1_1: {}'.format(net.blobs['conv1_1'].data.shape))
+print(net.blobs['conv1_1'].data)
+
+
